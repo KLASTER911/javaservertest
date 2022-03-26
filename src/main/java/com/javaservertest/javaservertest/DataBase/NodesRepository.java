3db1847a3f0fab16464fb24839ceca3e41da096a
Artem <kostishin.artiom@yandex.ru>
2022-03-26 21:38:38 +0300

    Добавлены кастомные команды БД.

--- src/main/java/com/javaservertest/javaservertest/DataBase/NodesRepository.java
+++ src/main/java/com/javaservertest/javaservertest/DataBase/NodesRepository.java
@@ -1,6 +1,22 @@
 package com.javaservertest.javaservertest.DataBase;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface NodesRepository extends CrudRepository<Nodes, Integer> {
 
+    @Query("SELECT t FROM Nodes t WHERE t.parentId = ?1") //можно и без аннотации Query
+    Optional<Nodes> findByParentId(Object parentId);
+
+    @Query("SELECT t FROM Nodes t WHERE t.name = ?1") //можно и без аннотации Query
+    Optional<Nodes> findByName(String name);
+
+    @Query("SELECT t FROM Nodes t WHERE t.ip = ?1") //можно и без аннотации Query
+    Optional<Nodes> findByIp(String ip);
+
+    @Query("SELECT t FROM Nodes t WHERE t.port = ?1") //можно и без аннотации Query
+    Optional<Nodes> findByPort(Integer port);
+
+    void deleteByParentId(Integer parentId);
 }
